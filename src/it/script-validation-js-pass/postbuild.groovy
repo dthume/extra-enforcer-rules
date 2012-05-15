593bf44cc69b6c57de90cdda18bf7a1cb40c8959
David Thomas Hume <dth@dthu.me>
2012-05-15 21:34:08 +0100

    Removed trailing whitespace

--- src/it/script-validation-js-pass/postbuild.groovy
+++ src/it/script-validation-js-pass/postbuild.groovy
@@ -2,4 +2,4 @@ def buildLog = new File(basedir, "build.log").text;
 def cachedMatches = buildLog =~ /INVOKER_IT_TEST_MARKER_CACHED/;
 def uncachedMatches = buildLog =~ /INVOKER_IT_TEST_MARKER_NONCACHED/;
 
-return 1 == cachedMatches.size() && 2 == uncachedMatches.size(); 
+return 1 == cachedMatches.size() && 2 == uncachedMatches.size();
