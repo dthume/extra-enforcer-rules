3bb7163ab30e6aecf349b46398609a714fd4d917
David Thomas Hume <dth@dthu.me>
2012-05-15 21:14:15 +0100

    Corrected validation test to ensure failure if rules do not cache successfully

---
+++ src/it/script-validation-js-pass/postbuild.groovy
@@ -0,0 +1,5 @@
+def buildLog = new File(basedir, "build.log").text;
+def cachedMatches = buildLog =~ /INVOKER_IT_TEST_MARKER_CACHED/;
+def uncachedMatches = buildLog =~ /INVOKER_IT_TEST_MARKER_NONCACHED/;
+
+return 1 == cachedMatches.size() && 2 == uncachedMatches.size(); 
